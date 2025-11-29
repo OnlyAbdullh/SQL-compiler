70ccac845dc816244c80c82dd784ed31ef7e8495
abdulrahman_204 <abdalrahmanaljomaat@gmail.com>
2025-11-29 12:17:27 +0300

    another commit

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ import sys
 from antlr4 import FileStream
 from generated.SQLLexer import SQLLexer
 
-from utils.error_listener import CollectingErrorListener, print_errors
 
 
 def main():
@@ -14,20 +13,14 @@ def main():
 
     lexer = SQLLexer(input_stream)
 
-    # Attach custom error listener
-    error_listener = CollectingErrorListener()
-    lexer.removeErrorListeners()
-    lexer.addErrorListener(error_listener)
-
-    # Process tokens
     tokens = lexer.getAllTokens()
+    
     for token in tokens:
+        print(token)
         token_name = lexer.symbolicNames[token.type]
-        print(f"{token_name:15} -> {token.text}")
+        print(f"{token_name:15} -> {token.text}\n")
 
-    # Print all errors at the end
-    print_errors(error_listener.errors)
-
+    
 
 if __name__ == "__main__":
     main()
